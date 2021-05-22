bc8ba99e888de8ceccf3a1427fd428ae947d7c8b
Pavel <pasha_rakov@bk.ru>
2021-05-22 23:35:16 +0700

    Выполнил 5 урок

--- lesson4-1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-digitOne = int(input('Введите первое целое число: '))
-digitTwo = int(input('Введите второе целое число: '))
-
-result = digitOne ** 2 + digitTwo ** 2
-
-print(result)
