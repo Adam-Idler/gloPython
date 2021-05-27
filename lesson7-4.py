c60b86bd3b57e24efbcd82870315e32a81ae4f23
Adam-Idler <69643159+Adam-Idler@users.noreply.github.com>
2021-05-27 17:07:57 +0700

    Update lesson7-4.py

--- lesson7-4.py
+++ lesson7-4.py
@@ -6,5 +6,6 @@ range2 = int(input())
 if range2 < range1:
     range2, range1 = range1, range2
 
-for i in range(range1, range2 + 1, 2):
-    print(i)
+for i in range(range1, range2 + 1):
+    if i % 2 == 0:
+        print(i)
